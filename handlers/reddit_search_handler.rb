6e3594bc72c0675ef99b514aee13e7031882a829
Dalf32 <kylepmullins@gmail.com>
2017-03-09 01:19:18 -0500

    Reworked the Reddit search so that it is can be threaded to improve performance. There is still an issue with logging from within the Futures though

--- handlers/reddit_search_handler.rb
+++ handlers/reddit_search_handler.rb
@@ -3,6 +3,7 @@
 # Author::	Kyle Mullins
 
 require 'redd'
+require 'concurrent'
 
 class RedditSearchHandler < CommandHandler
   feature :reddit, default_enabled: false
@@ -17,32 +18,76 @@ class RedditSearchHandler < CommandHandler
   def search_reddit(event, *criteria)
     subs_split = subs.shuffle.each_slice(config.subs_per_request).to_a
 
-    chosen_result = subs_split.each do |sub_list|
-      begin
-        results = reddit.subreddit(sub_list.join('+')).search(criteria.join(' '), sort: :relevance, limit: config.results_per_request)
-        results = results.select{ |e| !e.thumbnail.nil? && e.thumbnail != 'self' } if config.media_only
+    found_results_latch = Concurrent::CountDownLatch.new
+    request_complete_latch = Concurrent::CountDownLatch.new(subs_split.length)
+    search_complete_latch = Concurrent::CountDownLatch.new
 
-        break results.sample unless results.empty?
-        log.debug('Request returned no results.')
-      rescue Redd::ServerError => server_err
-        log.error("Reddit error: #{server_err.response.body}")
-        return "HTTP #{server_err.response.code} returned from Reddit API"
+    futures = subs_split.map { |sub_list|
+      Concurrent::Future.execute(pool: thread_pool) do
+        begin
+          results = reddit.subreddit(sub_list.join('+')).search(criteria.join(' '), sort: :relevance, limit: config.results_per_request)
+          results = results.select{ |e| !e.thumbnail.nil? && e.thumbnail != 'self' } if config.media_only
+
+          if results.empty?
+            # log.debug('Request returned no results.')
+            nil
+          else
+            found_results_latch.count_down
+            results.sample
+          end
+        rescue HTTP::TimeoutError => timeout
+          # log.error("Reddit error: #{timeout.response.body}")
+        ensure
+          request_complete_latch.count_down
+        end
       end
-    end
+    }
+
+    event.channel.start_typing
+
+    [found_results_latch, request_complete_latch].each{ |latch|
+      Concurrent::Future.execute do
+        latch.wait
+        search_complete_latch.count_down
+      end
+    }
+
+    search_complete_latch.wait
+    thread_pool.shutdown
+
+    found_results_latch.count_down
+    (0..request_complete_latch.count).each{ request_complete_latch.count_down }
+
+    completed_future = futures.find{ |future| future.fulfilled? && !future.value.nil? }
 
     preamble = "#{event.author.display_name} searched for \"#{criteria.join(' ')}\"."
-    return "#{preamble} No results were found." unless chosen_result.is_a?(Redd::Models::Submission)
+    return "#{preamble} No results were found." if completed_future.nil?
 
-    "#{preamble} Here's a result from /r/#{chosen_result.subreddit.to_h[:display_name]} with the title ***#{chosen_result.title}***\n#{chosen_result.url}"
-  end
-
-  def reddit
-    @reddit ||= Redd.it(client_id: config.client_id, secret: config.client_secret)
+    result = completed_future.value
+    "#{preamble} Here's a result from /r/#{result.subreddit.to_h[:display_name]} with the title ***#{result.title}***\n#{result.url}"
   end
 
   private
 
+  def reddit
+    @reddit ||= create_reddit_client
+  end
+
   def subs
     @@subs ||= open(config.subreddit_list_file).readlines.map{ |s| s.gsub('/r/', '').chomp }
   end
+
+  def thread_pool
+    @thread_pool ||= Concurrent::FixedThreadPool.new(config.concurrent_requests)
+  end
+
+  def create_reddit_client
+    Redd.it(client_id: config.client_id, secret: config.client_secret).tap do |reddit|
+      reddit.client.max_retries = 1
+    end
+  end
+end
+
+class Redd::APIClient
+  attr_accessor :max_retries
 end
